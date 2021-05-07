9708571184def746f5ab1e92a8c4497b741b8572
orion <81678710+cosmicdefi@users.noreply.github.com>
2021-05-07 02:45:20 -0700

    Update CosmicSwapToken.sol
    - Change file name to Universe.sol
    - Chnage token symbol to UNIVERSE

--- contracts/Universe.sol
+++ contracts/Universe.sol
@@ -4,7 +4,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract CosmicSwapToken is ERC20, Ownable {
-    constructor() public ERC20("CosmicSwap Token", "CST") { }
+    constructor() public ERC20("CosmicSwap", "UNIVERSE") { }
 
     function mint(address _to, uint256 _amount) public onlyOwner {
         _mint(_to, _amount);
